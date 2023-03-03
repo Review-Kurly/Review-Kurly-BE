6c7a52bbd395693692e5a6f9925d3d1b5f78109b
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-03-03 10:47:48 +0900

    feat: 프로젝트 초기 설정 적용 완료

---
+++ src/main/java/sparat/spartaclone/SpartaCloneApplication.java
@@ -0,0 +1,13 @@
+package sparat.spartaclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sparat/spartaclone/SpartaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package sparat.spartaclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
