d49fd1ff46958da519c8b6aa12fa7720bd1bcb87
dkssudtp1 <dkssudtp1@naver.com>
2023-03-03 17:41:30 +0900

    test : 풀리퀘스트 테스트

--- src/main/java/sparat/spartaclone/mainpage/controller/MainController.java
+++ src/main/java/sparat/spartaclone/mainpage/controller/MainController.java
@@ -1,5 +1,23 @@
 package sparat.spartaclone.mainpage.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import sparat.spartaclone.common.ApiResponse;
+import sparat.spartaclone.mainpage.dto.MainPageRequestDto;
+import sparat.spartaclone.mainpage.dto.MainPageResponseDto;
+import sparat.spartaclone.user.dto.SignupRequestDto;
+import sparat.spartaclone.user.dto.UserResponseDto;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class MainController {
-    //풀리퀘
+    @PostMapping("/main")
+    public ApiResponse<UserResponseDto> getList(MainPageRequestDto mainPageRequestDto) {
+        return ApiResponse.successOf(HttpStatus.CREATED, null);
+    }
+
 }
---
+++ src/main/java/sparat/spartaclone/mainpage/dto/MainPageRequestDto.java
@@ -0,0 +1,13 @@
+package sparat.spartaclone.mainpage.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MainPageRequestDto {
+    private String sort;
+    private String keyword;
+}
