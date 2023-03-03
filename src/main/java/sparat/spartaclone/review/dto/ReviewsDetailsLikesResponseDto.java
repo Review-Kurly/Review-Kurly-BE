6930275d5a698f08eb314c7d5c7edcadf4b860e2
Shanyu Li <uhaha529@gmail.com>
2023-03-03 20:48:32 +0900

    fix: dto schema 추가

--- src/main/java/sparat/spartaclone/review/dto/ReviewsDetailsLikesResponseDto.java
+++ src/main/java/sparat/spartaclone/review/dto/ReviewsDetailsLikesResponseDto.java
@@ -1,10 +1,13 @@
 package sparat.spartaclone.review.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class ReviewsDetailsLikesResponseDto {
+
+    @Schema(type = "integer", example = "25")
     private long likeCount;// 좋아요 개수
 }
