a6ae809c1463736f214b30ec69e74a9db39644e1
Shanyu Li <uhaha529@gmail.com>
2023-03-03 20:54:54 +0900

    fix: controller 수정

--- src/main/java/sparat/spartaclone/review/controller/ReviewController.java
+++ src/main/java/sparat/spartaclone/review/controller/ReviewController.java
@@ -31,13 +31,13 @@ public class ReviewController {
 
     @GetMapping("/{reviewsId}")
     @Operation(summary = "리뷰 상세페이지 조회", description = "리뷰 상세페이지 댓글 목록까지 조회 ")
-    public ApiResponse<ReviewsDetailsResponseDto> getReview(@PathVariable Long reviewId){
+    public ApiResponse<ReviewsDetailsResponseDto> getReview(@PathVariable Long reviewsId){
         return ApiResponse.successOf(HttpStatus.OK,null);
     }
 
     @PutMapping("/{reviewsId}")
     @Operation(summary = "리뷰 상세페이지 수정", description = "리뷰 상세페이지 수정 ")
-    public ApiResponse<ReviewsDetailsResponseDto> putReview(@PathVariable Long reviewId,
+    public ApiResponse<ReviewsDetailsResponseDto> putReview(@PathVariable Long reviewsId,
                                                           @RequestBody ReviewRequestDto requestDto,
                                                           HttpServletResponse response){
         return ApiResponse.successOf(HttpStatus.OK,null);
@@ -46,13 +46,13 @@ public class ReviewController {
 
     @DeleteMapping("/{reviewsId}")
     @Operation(summary = "리뷰 상세페이지 삭제", description = "리뷰 상세페이지 댓글 목록까지 삭제 ")
-    public ApiResponse<ReviewsDetailsResponseDto> deleteReview(@PathVariable Long reviewId, HttpServletResponse response){
+    public ApiResponse<ReviewsDetailsResponseDto> deleteReview(@PathVariable Long reviewsId, HttpServletResponse response){
         return ApiResponse.successOf(HttpStatus.OK, null);
     }
 
     @PostMapping("/likes/{reviewsId}")
     @Operation(summary = "좋아요 등록", description = "좋아요 등록 ")
-    public ApiResponse<ReviewsDetailsLikesResponseDto> toggleLikes(@PathVariable Long reviewId, HttpServletResponse response){
+    public ApiResponse<ReviewsDetailsLikesResponseDto> toggleLikes(@PathVariable Long reviewsId, HttpServletResponse response){
         return ApiResponse.successOf(HttpStatus.OK,null);
 
     }
