c1f8ccda4f03762221294223c9fe78d79a95751a
Shanyu Li <uhaha529@gmail.com>
2023-03-03 21:34:08 +0900

    fix: ReviewController에 "+ConstantTable.HEADER_NEEDED"

--- src/main/java/sparat/spartaclone/review/controller/ReviewController.java
+++ src/main/java/sparat/spartaclone/review/controller/ReviewController.java
@@ -3,9 +3,11 @@ package sparat.spartaclone.review.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ConstantPoolReader;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import sparat.spartaclone.common.ApiResponse;
+import sparat.spartaclone.common.constant.ConstantTable;
 import sparat.spartaclone.review.dto.ReviewRequestDto;
 import sparat.spartaclone.review.dto.ReviewDetailResponseDto;
 
@@ -21,7 +23,7 @@ public class ReviewController {
 
     //  리뷰상세페이지 작성
     @PostMapping("/")
-    @Operation(summary = "리뷰 상세페이지 작성", description ="리뷰 상세페이지 댓글 목록까지 작성" )
+    @Operation(summary = "리뷰 상세페이지 작성", description ="리뷰 상세페이지 댓글 목록까지 작성" + ConstantTable.HEADER_NEEDED)
     public ApiResponse<ReviewDetailResponseDto> createReview(@RequestBody ReviewRequestDto requestDto,
                                                              HttpServletResponse response
                                                        ){
@@ -35,7 +37,7 @@ public class ReviewController {
     }
 
     @PutMapping("/{reviewId}")
-    @Operation(summary = "리뷰 상세페이지 수정", description = "리뷰 상세페이지 수정 ")
+    @Operation(summary = "리뷰 상세페이지 수정", description = "리뷰 상세페이지 수정 " + ConstantTable.HEADER_NEEDED)
     public ApiResponse<ReviewDetailResponseDto> putReview(@PathVariable Long reviewId,
                                                           @RequestBody ReviewRequestDto requestDto,
                                                           HttpServletResponse response){
@@ -44,7 +46,7 @@ public class ReviewController {
     }
 
     @DeleteMapping("/{reviewId}")
-    @Operation(summary = "리뷰 상세페이지 삭제", description = "리뷰 상세페이지 댓글 목록까지 삭제 ")
+    @Operation(summary = "리뷰 상세페이지 삭제", description = "리뷰 상세페이지 댓글 목록까지 삭제 " + ConstantTable.HEADER_NEEDED)
     public ApiResponse<ReviewDetailResponseDto> deleteReview(@PathVariable Long reviewId, HttpServletResponse response){
         return ApiResponse.successOf(HttpStatus.OK, null);
     }
