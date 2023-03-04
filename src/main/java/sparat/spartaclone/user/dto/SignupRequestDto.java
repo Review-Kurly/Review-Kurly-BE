cd41ea45d4fc5e38aa48ff9e86b078ef1ce260f0
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-03-04 19:44:47 +0900

    fix: Change validation check

--- src/main/java/sparat/spartaclone/user/dto/SignupRequestDto.java
+++ src/main/java/sparat/spartaclone/user/dto/SignupRequestDto.java
@@ -14,22 +14,19 @@ import javax.validation.constraints.Size;
 @Getter
 @AllArgsConstructor
 public class SignupRequestDto {
-    @Size(min=4, max=10)
-    @Pattern(regexp = "/^(?=.*?[0-9])(?=.*?[a-z]).{6,16}$/")
+    @Pattern(regexp = "^(?=.*?[0-9])(?=.*?[a-z]).{6,16}$")
     @Schema(example = "username", description = "/^(?=.*?[0-9])(?=.*?[a-z]).{6,16}$/")
     private String username;
 
-    @Pattern(regexp = "/^[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9]{2,10}$/")
+    @Pattern(regexp = "^[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9]{2,10}$")
     @Schema(example = "nickname", description = "/^[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9]{2,10}$/")
     private String nickname;
 
-    @Size(min=8, max=15)
-    @Pattern(regexp = "/^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$/")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$")
     @Schema(example = "password", description = "/^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$/")
     private String password;
 
-    @Email
-    @Pattern(regexp = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
     @Schema(example = "user@gmail.com", description = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/")
     private String email;
 
