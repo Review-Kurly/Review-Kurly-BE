36b46e1e9bcba2e4dc7ea988599940e53c4f089d
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-03-05 21:22:22 +0900

    feat: Add description field to ReviewsDetailsResponseDto

--- src/main/java/sparat/spartaclone/common/config/WebSecurityConfig.java
+++ src/main/java/sparat/spartaclone/common/config/WebSecurityConfig.java
@@ -85,8 +85,8 @@ public class WebSecurityConfig implements WebMvcConfigurer {
                 .addMapping("/**") // 프로그램에서 제공하는 URL
                 .allowedOrigins("http://localhost:3000", "sparta.chit-chat.shop") // 요청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
                 .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE") // 어떤 메서드를 허용할 것인지 (GET, POST...)
-                .allowedHeaders("*", "Content-Type", "Authorization") // 어떤 헤더들을 허용할 것인지
-                .exposedHeaders("Authorization")
+                .allowedHeaders("Content-Type", "Authorization") // 어떤 헤더들을 허용할 것인지
+                .exposedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true) // 쿠키 요청을 허용한다(다른 도메인 서버에 인증하는 경우에만 사용해야하며, true 설정시 보안상 이슈가 발생할 수 있다)
                 .maxAge(3600); // preflight 요청에 대한 응답을 브라우저에서 캐싱하는 시간;
     }
