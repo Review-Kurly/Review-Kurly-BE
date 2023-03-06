67c412e4bd84307b8d0788346bf5a6438000574c
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-03-06 14:27:29 +0900

    feat: 토큰 유효 기간 1년으로 변경

--- src/main/java/sparat/spartaclone/common/jwt/JwtUtil.java
+++ src/main/java/sparat/spartaclone/common/jwt/JwtUtil.java
@@ -28,7 +28,7 @@ public class JwtUtil {
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String AUTHORIZATION_KEY = "auth";
     private static final String BEARER_PREFIX = "Bearer ";
-    private static final long TOKEN_TIME = 60 * 60 * 1000L;
+    private static final long TOKEN_TIME = 365 * 24 * 60 * 60 * 1000L;  // TODO: 현재는 임시적으로 이렇게 두고, 나중에 refresh token 을 저장할 필요가 있음
 
     @Value("${jwt.secret.key}")
     private String secretKey;
